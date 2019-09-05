3819cd1ab1158b32d5ba850606b77bc7b3c37526
yudan <yudan@gaoding.com>
2019-09-05 15:32:23 +0800

    chore: bump version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ git commit -m "[skip travis] Release"
 git tag $VERSION_TAG
 git push -f --tags https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git HEAD:master
 npm publish
-curl -X POST http://120.25.224.35:8081/version -d '{"name": "${PKG_NAME}","version": "${VERSION_TAG}"}'
+node ./release.js
