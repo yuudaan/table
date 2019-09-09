0e07b5825d07647ee73bca4e899cbfba603c5b66
yudan <yudan@gaoding.com>
2019-09-09 15:18:27 +0800

    chore: teamcity build

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -v
+
+echo "//npm.huanleguang.com/:_authToken=\${NPM_TOKEN}" >> .npmrc
+echo "//registry.npmjs.org/:_authToken=${PUBLISH_TOKEN}" >> .npmrc
+npm config set @gaoding:registry https://npm.huanleguang.com
+git config --global user.name "yudan"
+git config --global user.email "yudan@gaoding.com"
+export VERSION_TAG=`node -p "require('./package.json').version"`
+export PKG_NAME=`node -p "require('./package.json').name"`
+
+npm install
+npm run build
+
+
